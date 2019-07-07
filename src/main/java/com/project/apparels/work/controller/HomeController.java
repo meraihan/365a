dc3abaf7d8b7e1ec98329f7790c76bd44f94b5a4
meraihan <sayedmahmudraihan@gmail.com>
2019-07-07 10:16:48 -0700

    configuration added

--- src/main/java/com/project/apparels/work/controller/HomeController.java
+++ src/main/java/com/project/apparels/work/controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.project.apparels.work.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
+
+    @GetMapping("/home")
+    public String index(){
+        return "home/index";
+    }
 }
